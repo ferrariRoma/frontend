1169e47771bdc864e97df445bbcaeeec5f4f92ed
FerrariRoma <win9592@gmail.com>
2022-10-04 03:42:06 +0900

    Update : QueryClient defaultOptions 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+function queryErrorHandler<T>(error: T) {
+  console.log("⛔️ error connecting to server ⛔️");
+  return console.log(error);
+}
+
+// TODO : mutations 시 suspense는 defaultOptions로 설정이 안될까?
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true, // React.Suspense를 쓰기 위한 옵션
+      onError: queryErrorHandler, // error 전체 핸들링
+    },
+    mutations: {
+      onError: queryErrorHandler,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
-const queryClient = new QueryClient()
-
+// TODO : Suspense 사용할 수 없을까?
 root.render(
   <QueryClientProvider client={queryClient}>
     <App />
