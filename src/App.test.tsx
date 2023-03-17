d1ab44a6b3ea358e17ee2e70a5ac8fe48f79af5c
FerrariRoma <win9592@gmail.com>
2023-03-17 21:47:48 +0900

    Create : Welcome 생성 및 구글 로그인 버튼 public에 설정 #3

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders learn react link', () => {
+  describe('', () => {
+    it('렌더링', () => {
+      render(<App />);
+      const linkElement = screen.getByText(/learn react/i);
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
 });
