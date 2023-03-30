5ef048cec77b3737ea17187e567ff5d42faa8be6
FerrariRoma <win9592@gmail.com>
2023-03-30 11:50:14 +0900

    Update : login 안 했을 때 welcome 테스트 코드 수정 #3

--- src/test/components/Welcome.test.tsx
+++ src/test/components/Welcome.test.tsx
@@ -1,44 +1,73 @@
-import { render, screen } from '@testing-library/react';
+import { createEvent, fireEvent, render, screen } from '@testing-library/react';
+import { click } from '@testing-library/user-event/dist/click';
 import Welcome from '../../components/Welcome';
 
 describe('Welcome', () => {
   describe('유저가 로그인을 안했을 경우', () => {
-    it('타이틀을 렌더링을 한다.', () => {
+    it('타이틀과 로그인 버튼을 렌더링 한다.', () => {
       const { container } = render(
-        <div>
-          <h1>EXTREME TODO</h1>
+        <Welcome>
+          <Welcome.WelcomTitleAtom>EXTREME TODO</Welcome.WelcomTitleAtom>
+          <Welcome.GoogleLoginAtom />
+        </Welcome>
+      );
+
+      const googleImage = screen.getByRole('img');
+
+      expect(container).toHaveTextContent('EXTREME TODO');
+      expect(googleImage).toBeInTheDocument();
+      expect(googleImage).toHaveAttribute('alt', 'google login button');
+    });
+  });
+
+  describe('유저가 로그인을 했을 경우', () => {
+    it('로그아웃 버튼과 셋팅 버튼을 렌더링 한다.', () => {
+      const { container } = render(
+        <Welcome>
+          <Welcome.WelcomTitleAtom>EXTREME TODO</Welcome.WelcomTitleAtom>
           <div>
-            <img
-              src="../../public/2x/btn_google_signin_dark_pressed_web@2x.png"
-              alt="google login button"
-            ></img>
+            <button>logout</button>
+            <button>settings</button>
           </div>
-        </div>
+        </Welcome>
       );
 
       expect(container).toHaveTextContent('EXTREME TODO');
+      // FireLogic 두 개 만들기
     });
-
-    it('구글 로그인 ', () => {});
   });
 });
 
-// context
 /* 
-유저가 로그인을 안했을 경우
+context
+  유저가 로그인을 안했을 경우
+
+it
+  타이틀, 로그인 이미지(버튼)을 띄워준다.
+
+context
+  로그인 이미지를 누면 
+it
+  로그인 url로 넘어간다 or 갔다가 뭘 받아온다
 */
 
-// it
 /* 
-타이틀, 로그인 버튼을 띄워준다.
+context
+  유저가 로그인을 했을 경우
+
+it
+  타이틀, signout 버튼, setting 버튼 출력해주기
+
+it
+  signout 버튼을 누르면 signout로직이 작동된다.
+
+it
+  setting 버튼을 누르면 setting 모달창이 출력된다.
 */
 
-// context
 /* 
-유저가 로그인을 했을 경우
-*/
-
-// it
-/* 
-타이틀, signout 버튼, setting 버튼 출력해주기
+context
+  유저가 접속 시
+it
+  토큰을 탐지하고 로그인 이미지를 띄워줄지, 로그아웃/셋팅을 띄워줄지 결정한다. => 이건 custom Hook으로 뺄 거 같다.
 */
