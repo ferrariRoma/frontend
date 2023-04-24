766966a00923a1b8b37e0ba4e3560b18a823d600
FerrariRoma <win9592@gmail.com>
2023-04-24 22:09:20 +0900

    Create : BtnAtom 생성 #3

---
+++ src/atoms/BtnAtom.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+import { IChildProps } from '../shared/interfaces';
+
+// interface IBtnAtomProps extends IChildProps &&  {
+
+// }
+
+function BtnAtom() {
+  return <div></div>;
+}
+
+export default BtnAtom;
+
+const Btn = styled.div``;
--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,2 +1,3 @@
 export { default as GoogleLoginAtom } from './GoogleLoginAtom';
 export { default as TypoAtom } from './TypoAtom';
+export { default as BtnAtom } from './BtnAtom';
