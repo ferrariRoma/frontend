0fbb99066f57198c5c3bb1a408e41dc9d746c5c2
FerrariRoma <win9592@gmail.com>
2023-04-24 22:20:56 +0900

    Update : 컴파운드 패턴 적용 레벨을 Welcome 페이지에서 하기 위한 수정 #3
    세 단계 전 커밋의 연장선 입니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
+import { useEffect } from 'react';
 import Welcome from './components/Welcome';
 import { useCheckLogin } from './hooks';
-import { usersApi } from './shared/apis';
 
 function App() {
-  const handleLoginBtn = () => {
-    return usersApi.login();
-  };
-
-  const [isLogin, setIsLogin] = useCheckLogin();
-
-  return (
-    <Welcome>
-      <Welcome.TypoAtom fontSize="2rem"></Welcome.TypoAtom>
-      <Welcome.GoogleLoginAtom
-        handleLoginBtn={handleLoginBtn}
-      ></Welcome.GoogleLoginAtom>
-    </Welcome>
-  );
+  return <Welcome />;
 }
 
 export default App;
