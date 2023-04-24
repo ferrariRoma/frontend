18751e7cf883618054098eb24d888b2093b259b8
FerrariRoma <win9592@gmail.com>
2023-04-24 22:07:41 +0900

    Update : Welcome 페이지 리팩토링하기 #3
    이전처럼 작성을 하면 App.test에 모든 테스트가 작성되야 하기 때문에 테스트 코드를 각 컴포넌트 단위로 나누어 주기 위해서 수정함.

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,17 +1,45 @@
 import styled from '@emotion/styled';
-import { GoogleLoginAtom, TypoAtom } from '../atoms';
-import { IChildProps } from '../shared/interfaces';
+import { BtnAtom, GoogleLoginAtom, TypoAtom } from '../atoms';
+import { usersApi } from '../shared/apis';
+import { useCheckLogin } from '../hooks';
 
-const Welcome = ({ children }: IChildProps) => {
-  return <WelcomeContainer>{children}</WelcomeContainer>;
+interface IWelcome {
+  isLogin: string;
+  setIsLogin: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Welcome = () => {
+  const handleLoginBtn = async () => {
+    await usersApi.login();
+  };
+
+  const [isLogin, setIsLogin] = useCheckLogin();
+
+  return (
+    <>
+      <WelcomeContainer>
+        <TypoAtom fontSize="2rem">EXTREME TODO</TypoAtom>
+        {isLogin ? (
+          <BtnContainer>
+            <BtnAtom>logout</BtnAtom>
+            <BtnAtom>setting</BtnAtom>
+          </BtnContainer>
+        ) : (
+          <GoogleLoginAtom onClick={handleLoginBtn} />
+        )}
+      </WelcomeContainer>
+    </>
+  );
 };
 
 export default Welcome;
 
-Welcome.TypoAtom = TypoAtom;
-Welcome.GoogleLoginAtom = GoogleLoginAtom;
+// Welcome.TypoAtom = TypoAtom;
+// Welcome.GoogleLoginAtom = GoogleLoginAtom;
 
 const WelcomeContainer = styled.div`
   width: 100vw;
   height: 100vh;
 `;
+
+const BtnContainer = styled.div``;
