ca1b7d391e3e78a5ecd54cd7f5aa4a4c36419fa1
FerrariRoma <win9592@gmail.com>
2023-05-03 13:14:37 +0900

    Update : convention에 따라 ToggleAtom으로 명칭 변경 #3

---
+++ src/atoms/ToggleAtom.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+
+function ToggleAtom() {
+  return <div>Toggle</div>;
+}
+
+export default ToggleAtom;
+
+const Toggle = styled(motion.div)`
+  
+`
--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,4 +1,4 @@
 export { default as GoogleLoginAtom } from './GoogleLoginAtom';
 export { default as TypoAtom } from './TypoAtom';
 export { default as BtnAtom } from './BtnAtom';
-export { default as Toggle } from './Toggle';
+export { default as ToggleAtom } from './ToggleAtom';
