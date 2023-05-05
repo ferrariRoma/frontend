2c5addabccc7ceec6147545a5a9e9f9121160de3
FerrariRoma <win9592@gmail.com>
2023-05-05 19:41:51 +0900

    Update : 토글 버튼 초안 완성 #3
    희정님과 트러블 슈팅 및 피그마와 함께 토의하기

--- src/atoms/SwitchAtom.tsx
+++ src/atoms/SwitchAtom.tsx
@@ -9,22 +9,34 @@ interface ISwitchAtomProps {
 }
 
 function SwitchAtom({ value, setValue }: ISwitchAtomProps) {
-  const theme = useTheme<keyof typeof colorTheme>();
+  // QUESTION : 테마 적용 어떻게 하는건가요?
+  // const theme = useTheme();
+
   return (
     <SwitchContainter value={value} onClick={setValue}>
-      {<Switch layout>{value ? `on` : `off`}</Switch>}
+      <Switch layout>{value ? `on` : `off`}</Switch>
     </SwitchContainter>
   );
 }
 
 export default SwitchAtom;
 
+// TODO : 색 피그마에 맞게 수정하기
 const Switch = styled(motion.div)`
-  width: 0.306rem;
-`;
-const SwitchContainter = styled.div<Partial<ISwitchAtomProps>>`
-  width: 6.75rem;
+  width: 4.074375rem;
   height: 2.5rem;
+  border-radius: 1.2rem;
+  background-color: skyblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SwitchContainter = styled.div<Partial<ISwitchAtomProps>>`
+  width: 5.75rem;
+  height: 2.5rem;
+  border-radius: 3rem;
   display: flex;
   justify-content: ${(props) => (props.value ? 'flex-end' : 'flex-start')};
+  background-color: grey;
 `;
