20505aa7163ba7f644bac9898156e58753b3839e
imhjnoh <imhjnoh@gmail.com>
2023-05-12 00:31:42 +0900

    Create : CardAtom, TagAtom 생성 및 이미지 추가 #11

---
+++ src/atoms/CardAtom.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+// TODO: 나중에 일정한 사이즈를 가지게 되면 수정;
+const CardAtom = styled.div<{
+  padding?: string;
+  bg?: 'default' | 'transparent';
+}>`
+  font-size: 1rem;
+  background-color: white;
+  background: ${({ theme: { colors }, bg }) => {
+    switch (bg) {
+      case 'transparent':
+        return `linear-gradient(180deg, rgba(255, 255, 255, 0.71) 0%, rgba(255, 255, 255, 0) 100%);`;
+      default:
+        return `linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.85) 0%,
+      rgba(255, 255, 255, 0) 55.21%
+    ), ${colors.bgYellow}`;
+    }
+  }};
+  box-shadow: 0px 4px 62px rgba(0, 0, 0, 0.05);
+  border-radius: 30px;
+  padding: ${({ padding }) => padding ?? '3rem'};
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default CardAtom;
--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,3 +1,24 @@
+import styled from '@emotion/styled';
 export { default as GoogleLoginAtom } from './GoogleLoginAtom';
 export { default as TypoAtom } from './TypoAtom';
 export { default as BtnAtom } from './BtnAtom';
+export { default as CardAtom } from './CardAtom';
+export { default as TagAtom } from './TagAtom';
+
+// TODO: 작업이 끝나면 지워야하는 임시 타이포 아톰
+export const tmpTypo = styled.p<{
+  fosi?: string;
+  fowe?: string;
+  co?: string;
+  ba?: string;
+}>`
+  background-color: yellow;
+  font-size: ${({ fosi }) => fosi};
+  font-weight: ${({ fowe }) => fowe};
+  color: transparent;
+  background: ${({ ba, theme }) => ba ?? theme.colors.subFontColor};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+`;
