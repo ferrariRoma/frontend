9c3aca3240eec95c2162e2613c115556c495170e
FerrariRoma <win9592@gmail.com>
2023-05-13 04:35:29 +0900

    Create : 모달 컨테이너 작성 #3
    테스트 파일이 없는 이유는 useModal 훅에서 모달 컨테이너 안에 children을 렌더링한 결과도 같이  테스트 함

---
+++ src/components/ModalContainer.tsx
@@ -0,0 +1,9 @@
+import {IChildProps} from '../shared/interfaces'
+
+const ModalContainer = ({children}:IChildProps) => {
+  return (
+    <div>ModalContainer</div>
+  )
+}
+
+export default ModalContainer
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Welcome } from './Welcome';
 export { default as SettingModal } from './SettingModal';
+export { default as ModalContainer } from './ModalContainer';
