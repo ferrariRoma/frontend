e16d3e362b95460f01e3d99d5d37bb29c7d9e2df
imhjnoh <imhjnoh@gmail.com>
2023-06-12 19:36:20 +0900

    Create : RankingAndRecords 컴포넌트 추가 #11

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Welcome } from './Welcome';
+export { default as RankingAndRecords } from './RankingAndRecords';
+export type { IRankingAndRecordsProps } from './RankingAndRecords';
