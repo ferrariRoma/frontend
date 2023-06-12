f37384c4b33a906bb27726ce056c8b224e38ba72
imhjnoh <imhjnoh@gmail.com>
2023-06-12 19:37:14 +0900

    Update : Records 테스트 코드 수정 WIP #11

--- src/test/molecules/Records.test.tsx
+++ src/test/molecules/Records.test.tsx
@@ -1,29 +1,31 @@
 import { render } from '@testing-library/react';
 import React, { ReactNode } from 'react';
-import { Records, IRecordsProps } from '../../components';
+import { Records, IRecordsProps } from '../../molecules';
+import { ThemeProvider } from '@emotion/react';
+import { colorTheme } from '../../styles/theme';
+import { ITotalFocusTime } from '../../shared/interfaces';
 
 describe('Records', () => {
-  function renderRecords({ props }: IRecordsProps) {
+  function renderRecords(props: IRecordsProps) {
     return render(
-      <Records {...props}>
-        <Records.LogInToUnlock
-          label="로그인하고 나의 집중기록을 확인해보세요!"
-          navigate={jest.fn()}
-        />
-      </Records>,
+      <ThemeProvider theme={colorTheme}>
+        <Records {...props} />
+      </ThemeProvider>,
     );
   }
 
   describe('모든 경우에', () => {
     it('타이틀 텍스트를 렌더한다', () => {
-      const { getByText } = renderRecords({});
-      expect(getByText(/나의 집중 기록/)).not.toBeNull();
+      const fetchRecords = jest.fn();
+      const { getAllByText } = renderRecords({ isLogin: false, fetchRecords });
+      expect(getAllByText(/나의 집중 기록/)).not.toBeNull();
     });
   });
 
   describe('로그인 하지 않은 경우', () => {
     it('LogInToUnlock을 렌더하고, 데이터를 페치하지 않는다.', () => {
-      const { getByText } = renderRecords({});
+      const fetchRecords = jest.fn();
+      const { getByText } = renderRecords({ isLogin: false, fetchRecords });
       expect(
         getByText(/로그인하고 나의 집중 기록을 확인해보세요!/),
       ).not.toBeNull();
@@ -36,17 +38,19 @@ describe('Records', () => {
         daily: 207,
         weekly: 3098,
         monthly: -20325,
-      });
-      const { getByText } = renderRecords({ fetchRecords });
+      }) as unknown as Promise<ITotalFocusTime>;
+      const { getByText } = renderRecords({ fetchRecords, isLogin: false });
       expect(fetchRecords).toBeCalled();
       expect(getByText(/207/)).not.toBeNull();
-      expect(getByText(/3098/)).not.toBeNull();
-      expect(getByText(/-20325/)).not.toBeNull();
+      expect(getByText(/3,098/)).not.toBeNull();
+      expect(getByText(/-20,325/)).not.toBeNull();
     });
 
     it('기록 데이터를 페치에 실패하고 알린다.', () => {
-      const fetchRecords = jest.fn().mockRejectedValue('failed');
-      const { getByText } = renderRecords({ fetchRecords });
+      const fetchRecords = jest
+        .fn()
+        .mockRejectedValue('failed') as unknown as Promise<ITotalFocusTime>;
+      const { getByText } = renderRecords({ fetchRecords, isLogin: true });
       expect(fetchRecords).toBeCalled();
       expect(getByText(/데이터를 불러올 수 없습니다./)).not.toBeNull();
     });
