a7b0853f0f6ff984a923e2f5a4138400d0f1155e
FerrariRoma <win9592@gmail.com>
2023-06-16 01:04:07 +0900

    Update : 컴포넌트 파일에서 수정된 로그인 로직 적용 #21
    Co-Authored-By: imhjnoh <56210700+imhjnoh@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import { RankingAndRecords } from './components';
+import { useEffect } from 'react';
 import Welcome from './components/Welcome';
+import { setToken } from './hooks/useCheckLogin';
 
 function App() {
+  useEffect(() => {
+    const pathname = window.location.pathname;
+    if (Object.is(pathname, '/oauth')) {
+      const query = window.location.search;
+      const clientURL = new URLSearchParams(query);
+
+      const userinfo = {
+        email: clientURL.get('email'),
+        username: clientURL.get('username'),
+        extremeToken: clientURL.get('token'),
+      };
+
+      if (userinfo.extremeToken && userinfo.email) {
+        setToken(userinfo.extremeToken, userinfo.email);
+        history.replaceState('', '', process.env.REACT_APP_API_CLIENT_URL);
+      }
+
+      // TODO : 프론트엔드 웰컴 테스트 코드 확인 E2E
+      // TODO : 나중에 이슈로 만들기
+    }
+  }, []);
+
   return (
     <>
       <Welcome />
