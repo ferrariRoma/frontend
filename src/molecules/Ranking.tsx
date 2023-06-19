0fa30837a0df716321a81bc2d57583ea2648b457
imhjnoh <imhjnoh@gmail.com>
2023-06-20 05:59:11 +0900

    Update : LogInToUnlock 스타일 수정 #18
    - dummyCategories 적용

--- src/molecules/Ranking.tsx
+++ src/molecules/Ranking.tsx
@@ -7,6 +7,7 @@ import styled from '@emotion/styled';
 import RankingTexts from './RankingTexts';
 import { formatTime } from '../shared/utils';
 import CartegorySelector from './CartegorySelector';
+import { dummyCategories } from '../shared/constants';
 
 export interface IRankingProps extends IChildProps {
   fetchRanking: () => Promise<IRanking>;
@@ -27,96 +28,82 @@ function Ranking({ children, fetchRanking, isLogin }: IRankingProps) {
   }, []);
 
   return (
-    <RankingContainer>
-      <div
-        style={{
-          textAlign: 'left',
-          width: '100%',
-          gridColumn: '1/3',
-          gridRow: 'auto',
-          height: '100%',
-        }}
-      >
-        <Ranking.titleLabel fontSize="h3_bold" fontColor="titleColor">
-          카테고리 별 랭킹
-        </Ranking.titleLabel>
-      </div>
-
-      <RankingLeftContainer>
-        <RankingTexts>
-          <RankingTexts.Typo fontSize={'h3'}>이번 주</RankingTexts.Typo>
-          <div className="one_line">
-            <RankingTexts.Tag
-              styleOption={{
-                size: 'big',
-                bg: 'white',
-                fontsize: 'b2',
-                shadow: 'button_shadow',
-              }}
-            >
-              {'개발'}
-            </RankingTexts.Tag>
-            <RankingTexts.Typo fontSize={'h3'}>에</RankingTexts.Typo>
-          </div>
-          <div className="one_line time">
-            {formatTime(ranking?.user.time ?? 0)
-              .split(/(\d+)/)
-              .slice(1)
-              .map((text, idx) => {
-                return (
-                  <RankingTexts.Typo
-                    fontSize={idx % 2 === 0 ? 'h1' : 'h3_bold'}
-                  >
-                    {text}
-                  </RankingTexts.Typo>
-                );
-              })}
-          </div>
-          <RankingTexts.Typo fontSize={'h3'}>집중했어요.</RankingTexts.Typo>
-        </RankingTexts>
-        <CartegorySelector
-          categories={[
-            { name: '공부', id: 0 },
-            { name: '개발', id: 1 },
-            { name: '음악', id: 2 },
-            { name: '놀기', id: 3 },
-            { name: '업무', id: 4 },
-            { name: '장보기', id: 5 },
-            { name: '잘살기', id: 6 },
-            { name: '집안일', id: 7 },
-            { name: '정신수양', id: 8 },
-            { name: '필살기연구', id: 9 },
-            { name: '2023', id: 10 },
-            { name: '6월스터디', id: 11 },
-            { name: '7월스터디', id: 12 },
-            { name: '약속', id: 13 },
-            { name: '리액트', id: 14 },
-          ]}
-          selected={{ name: '개발', id: 1 }}
-          selectHandler={() => {
-            console.log('카테고리 클릭됨');
+    <>
+      <RankingContainer data-testid={'ranking-component'}>
+        <div
+          style={{
+            textAlign: 'left',
+            width: '100%',
+            gridColumn: '1/3',
+            gridRow: 'auto',
+            height: '100%',
           }}
-        />
-      </RankingLeftContainer>
-
-      <RankingRightContainer>
-        <Ranking.CardAtom
-          bg="transparent"
-          w="100%"
-          h="100%"
-          margin="0"
-          padding="1.5rem"
-          style={{ display: 'block', boxSizing: 'border-box' }}
         >
-          {ranking && (
-            <RankingChart
-              options={Object.keys(ranking?.group[0])}
-              // series={Object.values(ranking?.group[0])}
-              series={[1, 3, 2, 5, 4, 1, 1, 2, 3, 6, 2]}
-            />
-          )}
-        </Ranking.CardAtom>
-      </RankingRightContainer>
+          <Ranking.titleLabel fontSize="h3_bold" fontColor="titleColor">
+            카테고리 별 랭킹
+          </Ranking.titleLabel>
+        </div>
+
+        <RankingLeftContainer>
+          <RankingTexts>
+            <RankingTexts.Typo fontSize={'h3'}>이번 주</RankingTexts.Typo>
+            <div className="one_line">
+              <RankingTexts.Tag
+                styleOption={{
+                  size: 'big',
+                  bg: 'white',
+                  fontsize: 'b2',
+                  shadow: 'button_shadow',
+                }}
+              >
+                {'개발'}
+              </RankingTexts.Tag>
+              <RankingTexts.Typo fontSize={'h3'}>에</RankingTexts.Typo>
+            </div>
+            <div className="one_line time">
+              {formatTime(ranking?.user.time ?? 0)
+                .split(/(\d+)/)
+                .slice(1)
+                .map((text, idx) => {
+                  return (
+                    <RankingTexts.Typo
+                      fontSize={idx % 2 === 0 ? 'h1' : 'h3_bold'}
+                      key={idx + text}
+                    >
+                      {text}
+                    </RankingTexts.Typo>
+                  );
+                })}
+            </div>
+            <RankingTexts.Typo fontSize={'h3'}>집중했어요.</RankingTexts.Typo>
+          </RankingTexts>
+          <CartegorySelector
+            categories={dummyCategories}
+            selected={dummyCategories[1]}
+            selectHandler={() => {
+              console.log('카테고리 클릭됨');
+            }}
+          />
+        </RankingLeftContainer>
+
+        <RankingRightContainer>
+          <Ranking.CardAtom
+            bg="transparent"
+            w="100%"
+            h="100%"
+            margin="0"
+            padding="1.5rem"
+            style={{ display: 'block', boxSizing: 'border-box' }}
+          >
+            {ranking && (
+              <RankingChart
+                options={Object.keys(ranking?.group[0])}
+                series={Object.values(ranking?.group[0])}
+              />
+            )}
+          </Ranking.CardAtom>
+        </RankingRightContainer>
+      </RankingContainer>
       {!isLogin && (
         <LogInToUnlock
           navigate={() => {
@@ -125,7 +112,7 @@ function Ranking({ children, fetchRanking, isLogin }: IRankingProps) {
           }}
         />
       )}
-    </RankingContainer>
+    </>
   );
 }
 
