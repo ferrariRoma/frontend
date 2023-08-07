83c37bfa9f7782db55c35620a77f24a07b39f972
imhjnoh <imhjnoh@gmail.com>
2023-08-08 02:10:24 +0900

    Create : SideButtons molecule 생성 #38

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -2,3 +2,4 @@ export { default as RankingChart } from './RankingChart';
 export { default as LogInToUnlock } from './LogInToUnlock';
 export type { ILogInToUnlockProps } from './LogInToUnlock';
 export { default as Clock } from './Clock';
+export { default as SideButtons } from './SideButtons';
